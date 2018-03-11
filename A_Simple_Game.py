b82e8d2d11f0f8fe5b197c52cbc37c85442579b3
den200x <delu@ucdavis.edu>
2018-03-10 22:41:06 -0800

    updated with comments for SoloLearn

--- A_Simple_Game.py
+++ A_Simple_Game.py
@@ -1,7 +1,10 @@
+####simply concatenated the exsiting code from the learning path with a few changes indicated by comment 
+
+##########################part I
 def get_input():
   command = input(": ").split()
   verb_word = command[0]
-  if verb_word in verb_dict:
+  if verb_word in verb_dict:  #verb_dict is a dictionary which was created below
     verb = verb_dict[verb_word]
   else:
     print("Unknown verb {}". format(verb_word))
@@ -16,10 +19,8 @@ def get_input():
 def say(noun):
   return 'You said "{}"'.format(noun)
 
-verb_dict = {
-  "say": say,
-}
-############################
+
+############################part II
 class GameObject:
   class_name = ""
   desc = ""
@@ -31,19 +32,19 @@ class GameObject:
 
   def get_desc(self):
     return self.class_name + "\n" + self.desc
-
-class Goblin(GameObject):
+##I used ''' and''' to enclose the part which was causing some problems for the program as just a string 
+'''class Goblin(GameObject): 
   class_name = "goblin"
   desc = "A foul creature"
 
-goblin = Goblin("Gobbly")
+goblin = Goblin("Gobbly")''' ###this was moved to part III
 
 def examine(noun):
   if noun in GameObject.objects:
     return GameObject.objects[noun].get_desc()
   else:
     return "There is no {} here.".format(noun)
-############################################################
+############################################################part III
 class Goblin(GameObject):
   def __init__(self, name):
     self.class_name = "goblin"
@@ -67,6 +68,8 @@ class Goblin(GameObject):
   def desc(self, value):
     self._desc = value
 
+goblin = Goblin("Gobbly")
+
 def hit(noun):
   if noun in GameObject.objects:
     thing = GameObject.objects[noun]
@@ -80,3 +83,15 @@ def hit(noun):
     msg ="There is no {} here.".format(noun) 
   return msg
 
+###this part was moved from part I of the original code
+verb_dict = {
+  "say": say,
+  "examine": examine,
+  "hit": hit
+}
+
+while True:
+  get_input()
+  
+## not sure how to contiuniously run this with this Web Interface
+##if you run this on your Python Shell, there shouldn't be any EOFError
