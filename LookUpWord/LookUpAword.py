d6c3fc121b0f90bf571f8ca9c02a1c4c1a348785
den200x <delu@ucdavis.edu>
2018-03-12 17:01:04 -0700

    wordStranslationApp

---
+++ LookUpWord/LookUpAword.py
@@ -0,0 +1,18 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+    else:
+        return "The word doesn't exist. Please Try again"
+
+word = input("Enter word: ")
+
+print(translate(word))
+
+
